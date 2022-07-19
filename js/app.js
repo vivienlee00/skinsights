ed2573efe5c11dcd118ddf6bcff50519c10a93d7
Vivien Lee <vl236@cornell.edu>
2022-07-19 17:01:41 -0400

    quiz styling

--- js/app.js
+++ js/app.js
@@ -190,3 +190,9 @@ function showProducts(item, index){
             }
       }
 }
+
+function phoneMask() { 
+    var num = $(this).val().replace(/\D/g,''); 
+    $(this).val(num.substring(0,3) + '-' + num.substring(3,6) + '-' + num.substring(6,10)); 
+}
+$('[type="tel"]').focusout(phoneMask);
